cff72951cb3c128589d3d9352c61a2cc90faf422
dmitriyev-alexey <89267782+dmitriyev-alexey@users.noreply.github.com>
2022-10-20 12:21:10 +0000

    added comments

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket
 
 sock = socket.socket()
+print('Binding server...')
 sock.bind(('', 9090))
+print('Opening server for connections...')
 sock.listen(0)
+print('Server is ready to accept connections')
 conn, addr = sock.accept()
 print(addr)
 
 msg = ''
 
 while True:
+	print('Waiting for data from clients...')
 	data = conn.recv(1024)
 	if not data:
+		print('Closing connection...')
 		break
 	msg += data.decode()
+	print('Echoing data...')
 	conn.send(data)
 
+print('msg: ')
 print(msg)
-
+print('Closing server...')
 conn.close()
